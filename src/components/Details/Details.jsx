c0a57e82935357a4aa5d6ba59568943eb05ddf68
Bailey Fiedorowicz <bfiedorowicz1995@gmail.com>
2023-05-07 21:51:26 -0400

    changed styling on Details.jsx

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,23 +1,25 @@
 import { useSelector } from "react-redux";
-import { Grid } from "@mui/material";
+import { Grid, Container } from "@mui/material";
 
 function Details() {
 
     const movie = useSelector(store => store.movie);
 
     return (
-        <>
+        <Container>
             {/* <h1>{movie.title}</h1> */}
-            <Grid container spacing={2}>
+            <Grid style={{marginTop: '50px'}} container spacing={2}>
+                <Grid item xs={12}>
+                <h2>{movie.title}</h2>
+                </Grid>
                 <Grid item sm={6}>
-                    <h2>{movie.title}</h2>
                     <p style={{textAlign: 'left'}} >{movie.description}</p>
                 </Grid>
                 <Grid item sm={6}>
                     <img style={{ width: 'inherit', height: 'auto' }} src={movie.poster} />
                 </Grid>
             </Grid>
-        </>
+        </Container>
     )
 }
 
